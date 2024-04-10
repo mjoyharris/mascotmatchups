aaedb2aa9cd4d1027e3ef04c418335eea8056ce8
mjoyharris <mayajharris.com>
2024-04-10 19:29:00 +0000

    changed port number from 3000 3001

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(cors());
 app.use(express.static('assets')); // Serve assets
